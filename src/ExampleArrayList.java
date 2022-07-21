90ec5d42942ec97bb394b2f62c667f797631b074
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-07-21 19:25:40 +0300

    Update ExampleArrayList.java

--- src/ExampleArrayList.java
+++ src/ExampleArrayList.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ExampleArrayList {
+import java.util.ArrayList;
+
+public class ExampleArrayList {
+
+
+    public static void main(String[] args) {
+
+        // Создание экземпляра класса ArrayList с именем ссылки name
+        ArrayList<String> name = new ArrayList<String>();
+
+        // Добавление элементов в name с помощью метода add
+        name.add("Лада");
+        name.add("Волга");
+        name.add("Шевроле");
+        name.add("Мустанг");
+        name.add("Рено");
+
+        // Изменение элемента ArrayList с индексом 3
+        name.set(3, "Форд");
+
+        // Создание второго экземпляра класса ArrayList с именем ссылки name
+        ArrayList<String> nameJapan = new ArrayList<String>();
+        nameJapan.add("Сузуки");
+        nameJapan.add("Мазда");
+        nameJapan.add("Субару");
+
+        // Слияние коллекций
+        name.addAll(nameJapan);
+
+        System.out.println(name);
+    }
 }
+
